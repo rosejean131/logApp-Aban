5e397aafa3e4c6abfb7e6206c13f7f3e1c6a64b0
roseaban131 <112538382+roseaban131@users.noreply.github.com>
2022-10-13 19:53:15 +0800

    Delete inc directory

--- inc/navbar.php
+++
@@ -1,8 +0,0 @@
-
-<!-- Image and text -->
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">
-    <img src="img/bootstrap.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-    Log Application
-  </a>
-</nav>
